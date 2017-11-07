04a9a8b3dcd5440233fbc2054cf90dc94c78631e
ercarlso <erinjew@gmail.com>
2017-11-06 21:42:11 -0800

    Initial commit

---
+++ app/src/main/java/com/fhda/cs64a/tictactoe/MainActivity.java
@@ -0,0 +1,20 @@
+package com.fhda.cs64a.tictactoe;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void OnStartGame (View view) {
+        Intent newIntent = new Intent (this, GameBoard.class);
+        startActivity(newIntent);
+    }
+}
