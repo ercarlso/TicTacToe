ab293f31320542bd6181627cba8f0c070192bb4b
clustermass2@gmail.com <Madnesss2#>
2017-12-01 16:16:35 -0800

    computer gaming logic added.

--- app/src/main/java/com/fhda/cs64a/tictactoe/GameBoard.java
+++ app/src/main/java/com/fhda/cs64a/tictactoe/GameBoard.java
@@ -12,6 +12,7 @@ import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Random;
 
 public class GameBoard extends AppCompatActivity  {
     public String player1;
@@ -53,21 +54,23 @@ public class GameBoard extends AppCompatActivity  {
         builder.setPositiveButton("Play Again", new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int id) {
                 // Clear the game board ...
-                int white = Color.parseColor("#FFFFFF");
-                btn1.setText(""); btn1.setBackgroundColor(white);
-                btn2.setText(""); btn2.setBackgroundColor(white);
-                btn3.setText(""); btn3.setBackgroundColor(white);
-                btn4.setText(""); btn4.setBackgroundColor(white);
-                btn5.setText(""); btn5.setBackgroundColor(white);
-                btn6.setText(""); btn6.setBackgroundColor(white);
-                btn7.setText(""); btn7.setBackgroundColor(white);
-                btn8.setText(""); btn8.setBackgroundColor(white);
-                btn9.setText(""); btn9.setBackgroundColor(white);
+                int white = Color.parseColor("#FAFAFA");
+                btn1.setText(" "); btn1.setBackgroundColor(white);
+                btn2.setText(" "); btn2.setBackgroundColor(white);
+                btn3.setText(" "); btn3.setBackgroundColor(white);
+                btn4.setText(" "); btn4.setBackgroundColor(white);
+                btn5.setText(" "); btn5.setBackgroundColor(white);
+                btn6.setText(" "); btn6.setBackgroundColor(white);
+                btn7.setText(" "); btn7.setBackgroundColor(white);
+                btn8.setText(" "); btn8.setBackgroundColor(white);
+                btn9.setText(" "); btn9.setBackgroundColor(white);
                 txtMsgBottom.setText("");
                 turnNumber = 0;
+                currentPlayer = player2;
+                checkForWin();
             }
         });
-        builder.setNegativeButton("Quit", new DialogInterface.OnClickListener() {
+        builder.setNegativeButton("Back to main screen", new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int id) {
                 finish(); // Goes back to Welcome screen
             }
@@ -75,6 +78,7 @@ public class GameBoard extends AppCompatActivity  {
 
         AlertDialog dialog = builder.create();
         dialog.setCancelable(false);
+
         dialog.show();
 
     }
@@ -152,14 +156,20 @@ public class GameBoard extends AppCompatActivity  {
             } else if (!computerPlays && playerOnePlaysX && currentPlayer.equalsIgnoreCase(player2)) {
                 txtMsgTop.setText(player2 + "'s turn (o)");
             } else if (!computerPlays && !playerOnePlaysX && currentPlayer.equalsIgnoreCase(player2)) {
+                txtMsgTop.setText(player2 + "'s turn (x)");
+            } else if (computerPlays && playerOnePlaysX && currentPlayer.equalsIgnoreCase(player1)) {
                 txtMsgTop.setText(player1 + "'s turn (x)");
-            } else if (computerPlays && playerOnePlaysX) {
-                txtMsgTop.setText("Computer's turn (x)");
-                //TODO invoke computer play method
-            } else if (computerPlays && playerOnePlaysX) {
-                txtMsgTop.setText("Computer's turn (o)");
-                //TODO invoke computer play method
-            } else {
+            } else if (computerPlays && !playerOnePlaysX && currentPlayer.equalsIgnoreCase(player1)) {
+                txtMsgTop.setText(player1 + "'s turn (o)");
+            } else if (computerPlays && playerOnePlaysX && currentPlayer.equalsIgnoreCase(player2)){
+                txtMsgTop.setText(player2 + "'s turn (o)");
+                computerPlays();
+            }else if (computerPlays && !playerOnePlaysX && currentPlayer.equalsIgnoreCase(player2)){
+                txtMsgTop.setText(player2 + "'s turn (x)");
+                computerPlays();
+            }
+
+            else {
                 txtMsgTop.setText("Error occured, please, restart.");
             }
         }
@@ -170,7 +180,7 @@ public class GameBoard extends AppCompatActivity  {
             Button[] flashButtons =  winningPattern.get(winCombo);
 
             for(int x =2;x>=0; x--){
-                flashButtons[x].setBackgroundColor(Color.parseColor("#FF0000"));
+                flashButtons[x].setBackgroundColor(Color.parseColor("#17c762"));
             }
 
             this.displayScore(currentPlayer);
@@ -178,14 +188,187 @@ public class GameBoard extends AppCompatActivity  {
 
     };
 
+    public void computerPlays(){
 
-//    @Override
-//    public void onClick(View v) {
-//        String s = " R.id." + Integer.toString(v.getId());
-//        Button b = (Button)v;
-//        String buttonText = b.getText().toString();
-//        txtMsgTop.setText(buttonText);
-//    }
+
+        String computerPlaysLetter = "";
+        String player1PlaysLetter ="";
+        if(playerOnePlaysX){
+            computerPlaysLetter = "O";
+            player1PlaysLetter = "X";
+
+        }
+        else{
+            computerPlaysLetter = "X";
+            player1PlaysLetter = "O";
+        }
+
+        HashMap<Integer, Button[]> winningPattern = new HashMap<Integer, Button[]>();
+        winningPattern.put(0,new Button[]{btn1,btn2,btn3});
+        winningPattern.put(1,new Button[]{btn4,btn5,btn6});
+        winningPattern.put(2,new Button[]{btn7,btn8,btn9});
+        winningPattern.put(3,new Button[]{btn1,btn4,btn7});
+        winningPattern.put(4,new Button[]{btn2,btn5,btn8});
+        winningPattern.put(5,new Button[]{btn3,btn6,btn9});
+        winningPattern.put(6,new Button[]{btn1,btn5,btn9});
+        winningPattern.put(7,new Button[]{btn3,btn5,btn7});
+
+        ArrayList possibleCombinations = new ArrayList<String>();
+        possibleCombinations.add(btn1.getText().toString() + btn2.getText().toString() + btn3.getText().toString());
+        possibleCombinations.add(btn4.getText().toString() + btn5.getText().toString() + btn6.getText().toString());
+        possibleCombinations.add(btn7.getText().toString() + btn8.getText().toString() + btn9.getText().toString());
+        possibleCombinations.add(btn1.getText().toString() + btn4.getText().toString() + btn7.getText().toString());
+        possibleCombinations.add(btn2.getText().toString() + btn5.getText().toString() + btn8.getText().toString());
+        possibleCombinations.add(btn3.getText().toString() + btn6.getText().toString() + btn9.getText().toString());
+        possibleCombinations.add(btn1.getText().toString() + btn5.getText().toString() + btn9.getText().toString());
+        possibleCombinations.add(btn3.getText().toString() + btn5.getText().toString() + btn7.getText().toString());
+
+        // Search for player 1 two consecutive letters in winning combo
+        //And try to prevent player 1 from winning
+        int decision = 8;
+        for(int i = 0; i<8;i++ ){
+            if(possibleCombinations.get(i).toString().contains(player1PlaysLetter+player1PlaysLetter)){
+                decision = i;
+                break;
+            }
+        }
+
+
+        if(decision < 8){
+            if(possibleCombinations.get(decision).toString().equalsIgnoreCase(player1PlaysLetter + player1PlaysLetter+" ")){
+                Button[] buttons =  winningPattern.get(decision);
+                buttons[2].setText(computerPlaysLetter);
+                checkForWin();
+                return;
+
+            }
+            if(possibleCombinations.get(decision).toString().equalsIgnoreCase(player1PlaysLetter + " " +player1PlaysLetter)){
+                Button[] buttons =  winningPattern.get(decision);
+                buttons[1].setText(computerPlaysLetter);
+                checkForWin();
+                return;
+            }
+            if(possibleCombinations.get(decision).toString().equalsIgnoreCase(" " + player1PlaysLetter+player1PlaysLetter)){
+                Button[] buttons =  winningPattern.get(decision);
+                buttons[0].setText(computerPlaysLetter);
+                checkForWin();
+                return;
+            }
+
+        }
+
+        decision = 8;
+        for(int i = 0; i<8;i++ ){
+            if(possibleCombinations.get(i).toString().contains(computerPlaysLetter+computerPlaysLetter)){
+                decision = i;
+                break;
+            }
+        }
+
+        if(decision < 8){
+            if(possibleCombinations.get(decision).toString().equalsIgnoreCase(computerPlaysLetter + computerPlaysLetter+" ")){
+                Button[] buttons =  winningPattern.get(decision);
+                buttons[2].setText(computerPlaysLetter);
+                checkForWin();
+                return;
+
+            }
+            if(possibleCombinations.get(decision).toString().equalsIgnoreCase(computerPlaysLetter + " " +computerPlaysLetter)){
+                Button[] buttons =  winningPattern.get(decision);
+                buttons[1].setText(computerPlaysLetter);
+                checkForWin();
+                return;
+            }
+            if(possibleCombinations.get(decision).toString().equalsIgnoreCase(" " + computerPlaysLetter+computerPlaysLetter)){
+                Button[] buttons =  winningPattern.get(decision);
+                buttons[0].setText(computerPlaysLetter);
+                checkForWin();
+                return;
+            }
+
+        }
+
+        decision = 8;
+        ArrayList<Integer> combinations = new ArrayList<>();
+        for(int i = 0; i<8;i++ ){
+            if(possibleCombinations.get(i).toString().contains(computerPlaysLetter)){
+                combinations.add(i);
+            }
+        }
+        if(!combinations.isEmpty()){
+            for (int i = combinations.size(); i>=0;i--){
+                Button[] buttons =  winningPattern.get(i);
+                if(buttons[0].getText().toString().equalsIgnoreCase(computerPlaysLetter) && buttons[1].getText().toString().equalsIgnoreCase(" ")){
+                    buttons[1].setText(computerPlaysLetter);
+                    checkForWin();
+                    return;
+                }
+                if(buttons[0].getText().toString().equalsIgnoreCase(computerPlaysLetter) && buttons[2].getText().toString().equalsIgnoreCase(" ")){
+                    buttons[2].setText(computerPlaysLetter);
+                    checkForWin();
+                    return;
+                }
+                if(buttons[1].getText().toString().equalsIgnoreCase(computerPlaysLetter) && buttons[0].getText().toString().equalsIgnoreCase(" ")){
+                    buttons[0].setText(computerPlaysLetter);
+                    checkForWin();
+                    return;
+                }
+                if(buttons[1].getText().toString().equalsIgnoreCase(computerPlaysLetter) && buttons[2].getText().toString().equalsIgnoreCase(" ")){
+                    buttons[2].setText(computerPlaysLetter);
+                    checkForWin();
+                    return;
+                }
+                if(buttons[2].getText().toString().equalsIgnoreCase(computerPlaysLetter) && buttons[1].getText().toString().equalsIgnoreCase(" ")){
+                    buttons[1].setText(computerPlaysLetter);
+                    checkForWin();
+                    return;
+                }
+                if(buttons[2].getText().toString().equalsIgnoreCase(computerPlaysLetter) && buttons[0].getText().toString().equalsIgnoreCase(" ")){
+                    buttons[0].setText(computerPlaysLetter);
+                    checkForWin();
+                    return;
+                }
+            }
+        }
+        else{
+            //Empty gamepad
+            if(btn5.getText().toString().equalsIgnoreCase(" ")) {
+                btn5.setText(computerPlaysLetter);
+                checkForWin();
+                return;
+            }
+            else{
+                ArrayList<Button> buttonsArray = new ArrayList<>();
+                buttonsArray.add(btn1);
+                buttonsArray.add(btn2);
+                buttonsArray.add(btn3);
+                buttonsArray.add(btn4);
+                buttonsArray.add(btn5);
+                buttonsArray.add(btn6);
+                buttonsArray.add(btn7);
+                buttonsArray.add(btn8);
+                buttonsArray.add(btn9);
+
+                while(true){
+                    Random ran = new Random();
+                    int x = ran.nextInt((9)+1);
+                    if(buttonsArray.get(x).getText().toString().equalsIgnoreCase(" ")) {
+                        buttonsArray.get(x).setText(computerPlaysLetter);
+                        checkForWin();
+                        return;
+                    }
+                }
+
+
+
+            }
+        }
+
+
+
+    }
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -193,32 +376,36 @@ public class GameBoard extends AppCompatActivity  {
         txtMsgTop = (TextView) findViewById(R.id.txtMsgTop);
         txtMsgBottom = (TextView) findViewById(R.id.txtMsgBottom);
         btn1  = (Button) findViewById(R.id.btn1);
-
+        btn1.setText(" ");
         btn2  = (Button) findViewById(R.id.btn2);
-
+        btn2.setText(" ");
         btn3  = (Button) findViewById(R.id.btn3);
-
+        btn3.setText(" ");
         btn4  = (Button) findViewById(R.id.btn4);
-
+        btn4.setText(" ");
         btn5  = (Button) findViewById(R.id.btn5);
-
+        btn5.setText(" ");
         btn6  = (Button) findViewById(R.id.btn6);
-
+        btn6.setText(" ");
         btn7  = (Button) findViewById(R.id.btn7);
-
+        btn7.setText(" ");
         btn8  = (Button) findViewById(R.id.btn8);
-
+        btn8.setText(" ");
         btn9  = (Button) findViewById(R.id.btn9);
-
+        btn9.setText(" ");
         //This is data needed from welcome screen.
         player1 = "Steven";
         player2 = "Maya";
-        playerOnePlaysX = true;
+        playerOnePlaysX = false;
         computerPlays = false;
 
         //This is data needed from welcome screen.
 
         currentPlayer = player2;
+
+        if(computerPlays){
+            player2 = "Computer";
+        }
 
         // Needed for score board
         builder = new AlertDialog.Builder(this);
@@ -253,7 +440,7 @@ public class GameBoard extends AppCompatActivity  {
                 }
 
                 else{
-                    Toast.makeText(getApplicationContext(),"Please, select blank spot!", Toast.LENGTH_LONG).show();
+                    Toast.makeText(getApplicationContext(),"Please, select blank spot!", Toast.LENGTH_SHORT).show();
                 }
 
             }
